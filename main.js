954a3a553a54631d297dba5294b90613b085e698
Lily097 <98lillitlu13@gmail.com>
2020-09-14 19:37:50 +0500

    Редактирование карточек без сохранения в localStorage

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ var isEditMode = false;
 // массив с данными карточек
 var cards = [];
 
+// массив с историей изменений
+var changes = [];
+
 // id редактируемой карты
 var editCardId = null;
+
+var maxCardId = 0;
+
+var fieldNames = [
+    "occupation",
+    "responsibilities",
+    "startDate",
+    "endDate"
+];
 
 function switchButtons(isEditMode) {
     if (isEditMode) {
@@ -19,21 +31,116 @@ function switchButtons(isEditMode) {
     }
 }
 
+function startEditing() {
+    isEditMode = true;
+    switchButtons(isEditMode);
+    $('#my-job').addClass('my-job-edit');
+}
+
+function finishEditing() {
+    isEditMode = false;
+    switchButtons(isEditMode);
+    $('#my-job').removeClass('my-job-edit');
+    if (editCardId != null) {
+        // если было добавленое новой карты, удалим
+        if (editCardId == -1) {
+            cards = cards.filter(x => x.id != -1);
+        }
+        editCardId = null;
+        renderJobCards();
+    }
+}
+
+function addNewCard() {
+    // если какая-то карточка уже редактируется, запрещаем добавление
+    if (editCardId != null) {
+        return;
+    }
+
+    var newCard = {
+        id: -1
+    };
+    fieldNames.forEach(fieldName => newCard[fieldName] = '');
+    cards.push(newCard);
+
+    // -1 означает что это новая карточка
+    editCardId = -1;
+
+    // обновить DOM
+    renderJobCards();
+}
+
+// начать редактирование карточки с id=cardId
 function editCard(cardId) {
     editCardId = cardId;
     renderJobCards();
 }
 
+// удаление карточки с id=cardId
 function removeCard(cardId) {
-    console.log(cardId);
+    // добавляем изменение в массив
+    changes.push({
+        "type": "remove",
+        "id": cardId
+    });
+
+    // TODO сохранить массив изменений в localStorage
+    
+    // применяем изменение к текущему состоянию
+    cards = cards.filter(x => x.id != cardId);
+    
+    // обновить DOM
+    renderJobCards();
 }
 
+// сохранить изменения в карточке с id=cardId
 function saveChanges(cardId) {
+    // обновленный объект карточки
+    var updatedCard = {
+        id: cardId
+    };
+    fieldNames.forEach(fieldName => {
+        var value = $(`[data-card-id="${cardId}"] [name="${fieldName}"]`).val();
+        updatedCard[fieldName] = value;
+    });
 
+    if (cardId == -1) {
+        // это новая карточка. присвоить новый идентификатор
+        maxCardId++;
+        // TODO сохранить в localStorage
+        updatedCard.id = maxCardId;
+    }
+
+    // подменяем редактируемую карту новой версией
+    cards = cards.map(card => {
+        if (card.id != cardId) {
+            return card;
+        } else {
+            return updatedCard;
+        }
+    });
+
+    // сохранить изменение в массив
+    changes.push({
+        ...updatedCard,
+        type: 'edit'
+    });
+
+    // редактирование завершено
+    editCardId = null;
+
+    // обновить DOM
+    renderJobCards();
 }
 
-function cancelAddEditCard(card) {
-
+// отменить внесение изменений в карточку с id=cardId
+function cancelAddEditCard(cardId) {
+    if (editCardId == -1) {
+        // новая карточка не была сохранена - удалить ее из массива
+        cards = cards.filter(x => x.id != -1);
+    }
+    editCardId = null;
+    renderJobCards();
 }
 
 // обновить карточки
@@ -47,7 +154,7 @@ function renderJobCards() {
             res += renderJobCard(job);
         }
     });
-    console.log(res);
+    //console.log(res);
     $("#cards").html(res);
 }
 
@@ -92,22 +199,18 @@ function renderJobCardEdit(card) {
 }
 
 function init() {
-    $('#btn-start-editing').click(() => {
-        isEditMode = true;
-        switchButtons(isEditMode);
-        $('#my-job').addClass('my-job-edit');
-    });
-
-    $('#btn-finish-editing').click(() => {
-        isEditMode = false;
-        switchButtons(isEditMode);
-        $('#my-job').removeClass('my-job-edit');
-    });
+    $('#btn-start-editing').click(startEditing);
+    $('#btn-finish-editing').click(finishEditing);
+    $('#btn-add-new-card').click(addNewCard);
 
     fetch("/data.json")
         .then((resp) => resp.json())
         .then((data) => {
             cards = data;
+            var ids = cards.map(x => x.id);
+            var maxIdFromData = Math.max.apply(null, ids);
+            maxCardId = Math.max(maxCardId, maxIdFromData);
+            // TODO сохранить/прочитать maxCardId из localStorage
             // TODO подмешать изменения из localStorage
             renderJobCards();
         });
